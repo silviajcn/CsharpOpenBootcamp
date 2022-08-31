7a59bb747648d93d60b8d8a2ac617ed0ee7fbb72
Silvia <silviajcn@gmail.com>
2022-08-31 14:43:49 -0500

    first commit

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Ejercicio 1
+Console.WriteLine("Hola!, ingresa tu nombre, por favor");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Tu nombre es: " + nombre);
+
+Console.WriteLine();
+
+// Ejercicio 2
+Console.WriteLine("Fecha y Hora actual:");
+Console.WriteLine(DateTime.Now);
---
+++ Ejercicio1/README.md
@@ -0,0 +1,9 @@
+# Ejercicios sesión 1
+
+## Ejercicio 1
+
+Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+## Ejercicio 2
+
+Escribe un programa que tome la hora y la escriba por pantalla.
